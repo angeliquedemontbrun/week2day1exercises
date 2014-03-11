9b0b491116dddc47812e14e47f755330a4858e44
Angelique de Montbrun <angeliquedemontbrun@me.com>
2014-03-10 21:17:34 -0400

    files

---
+++ app1.rb
@@ -0,0 +1,15 @@
+require 'sinatra'
+require 'sinatra/reloader'
+
+get '/' do
+	erb :index
+end
+
+get '/hello/:name' do
+	name = params[:name]
+
+	erb :hello, locals: {
+		some_name: name,
+		x: Time.now
+	}
+end
---
+++ w2d1 ex.rb
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Broken Apps</title>
+</head>
+<body>
+<p> Hello World </p> 
+</body>
+</html>
